67f753be4b3423ca3da0cbff86ef20658dd27e28
a7u7a <esteban@esrs.co>
2024-09-12 15:48:35 +0200

    Nuevo script autostart

---
+++ run_yfinance_app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /home/pi/rpi-rgb-led-matrix/bindings/python/samples/bloko/yfinance_env/bin/activate
+exec python /home/pi/rpi-rgb-led-matrix/bindings/python/samples/bloko/async_finance.py
